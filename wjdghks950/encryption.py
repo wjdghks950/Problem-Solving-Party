743e17e48953eda637256d29e3d95dfdb53fdef1
Jeonghwan Kim <jeonghwankim123@gmail.com>
2022-10-11 16:17:01 +0900

    Solution to Encryption - hackrRank

--- wjdghks950/encryption.py
+++ wjdghks950/encryption.py
@@ -14,7 +14,20 @@ import sys
 #
 
 def encryption(s):
-    # Write your code here
+    l = len(s)
+    col = math.ceil(math.sqrt(l))
+    row = math.floor(math.sqrt(l))
+    if l > col**2 - col:
+        row = col
+    ans = ""
+    for c in range(col):
+        ans += s[c]
+        if l <= c + ((row-1) * col):
+            row = row - 1
+        for r in range(1,row):
+            ans = ans + s[c + (r * col)]
+        ans += " "
+    return ans
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
